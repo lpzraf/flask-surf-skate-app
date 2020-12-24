96de181442ebdaf16974d60f0c675253b6e47638
Rafael Lopez <lpz.raf@gmail.com>
2020-12-24 16:31:39 -0400

    Added temp fix to have users in 2 cards of the index temnplate. Need to update when the len of the 2 dimensional array is odd. At the moment im hiding user 1 when odd.

--- app.py
+++ app.py
@@ -2,18 +2,29 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_modus import Modus
 from dotenv import load_dotenv
 import os
-from user import User
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_CREDENTIALS')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 modus = Modus(app)
 
-users = []
 
+class User(db.Model):
 
-def find_user(user_id):
-    return [user for user in users if user.id == user_id][0]
+    __tablename__: 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
 
 
 # @app.route('/')
@@ -28,35 +39,46 @@ def root():
 
 @app.route('/users', methods=['GET', 'POST'])
 def index():
+    users = User.query.all()
+    users_tuple = []
+    if len(users) % 2 != 0:
+        users = users[1:]
+    for user1, user2 in zip(users[0::2], users[1::2]):
+        users_tuple.append([user1, user2])
+
     if request.method == 'POST':
         new_user = User(request.form['first_name'], request.form['last_name'])
-        users.append(new_user)
+        db.session.add(new_user)
+        db.session.commit()
         return redirect(url_for('index'))
-    return render_template('index.html', users=users)
+    return render_template('users/index.html', users=users_tuple)
 
 
 @app.route('/users/new')
 def new():
-    return render_template('new.html')
+    return render_template('users/new.html')
 
 
 @app.route('/users/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def show(id):
-    found_user = find_user(id)
+    found_user = User.query.get(id)
     if request.method == b'PATCH':
         found_user.first_name = request.form['first_name']
         found_user.last_name = request.form['last_name']
+        db.session.add(found_user)
+        db.session.commit()
         return redirect(url_for('index'))
     if request.method == b"DELETE":
-        users.remove(found_user)
+        db.session.delete(found_user)
+        db.session.commit()
         return redirect(url_for('index'))
-    return render_template('show.html', user=found_user)
+    return render_template('users/show.html', user=found_user)
 
 
 @app.route('/users/<int:id>/edit')
 def edit(id):
-    found_user = find_user(id)
-    return render_template('edit.html', user=found_user)
+    found_user = User.query.get(id)
+    return render_template('users/edit.html', user=found_user)
 
 
 @app.route('/surf-map')
