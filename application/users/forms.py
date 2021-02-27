9ba6677a6820c04433bde4b79e4fcedd61ef4201
Rafael Lopez <lpz.raf@gmail.com>
2021-02-26 21:27:39 -0400

    Added followers func.

--- application/users/forms.py
+++ application/users/forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, validators
+from wtforms import StringField, PasswordField, validators, SubmitField
 
 
 class UserForm(FlaskForm):
     username = StringField('Username', [validators.DataRequired()])
     password = PasswordField('Password', [validators.DataRequired()])
+    first_name = StringField('First Name', [validators.DataRequired()])
+    last_name = StringField('Last Name', [validators.DataRequired()])
 
 
 class DeleteForm(FlaskForm):
@@ -14,3 +16,8 @@ class DeleteForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField('Username', [validators.DataRequired()])
     password = PasswordField('Password', [validators.DataRequired()])
+
+
+# follow or unfollow form
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
--- application/users/routes.py
+++ application/users/routes.py
@@ -1,7 +1,7 @@
 from flask import (render_template, request,
                    redirect, url_for, session,
                    flash, Blueprint, g)
-from application.users.forms import UserForm, DeleteForm
+from application.users.forms import UserForm, DeleteForm, EmptyForm
 from application.models import User
 from application import db, bcrypt
 import datetime
@@ -38,6 +38,8 @@ def signup():
         try:
             new_user = User(form.username.data,
                             form.password.data,
+                            form.first_name.data,
+                            form.last_name.data,
                             datetime.datetime.now())
             db.session.add(new_user)
             db.session.commit()
@@ -95,3 +97,45 @@ def show(user_id):
     return render_template('users/show.html',
                            user=found_user,
                            delete_form=delete_form)
+
+# follow and unfollow routes
+@users_bp.route('/follow/<int:user_id>', methods=['POST'])
+@ensure_authenticated
+def follow(user_id):
+    form = EmptyForm()
+    curr_user_obj = g.user
+    found_user = User.query.get(user_id)
+    if form.validate_on_submit():
+        if found_user is None:
+            flash('User not found!', 'negative')
+            return redirect(url_for('users.index'))
+        if found_user == curr_user_obj:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('users.show', user_id=found_user.id))
+        curr_user_obj.follow(found_user)
+        db.session.commit()
+        flash(f'You are following {found_user.username}!', 'positive')
+        return redirect(url_for('users.show', user_id=found_user.id))
+    else:
+        return redirect(url_for('users.index'))
+
+
+@users_bp.route('/unfollow/<int:user_id>', methods=['POST'])
+@ensure_authenticated
+def unfollow(user_id):
+    form = EmptyForm()
+    found_user = User.query.get(user_id)
+    curr_user_obj = g.user
+    if form.validate_on_submit():
+        if found_user is None:
+            flash('User not found!', 'negative')
+            return redirect(url_for('users.index'))
+        if found_user == curr_user_obj:
+            flash('You cannot unfollow yourself!', 'negative')
+            return redirect(url_for('users.show', user_id=found_user.id))
+        curr_user_obj.unfollow(found_user)
+        db.session.commit()
+        flash(f'You are not following {found_user.username}!', 'negative')
+        return redirect(url_for('users.show', user_id=found_user.id))
+    else:
+        return redirect(url_for('users.index'))
